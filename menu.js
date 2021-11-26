9a2e2c8de948d2b8c2c9f78ad544a1f50448b1eb
JuanMartinMestica <juan.mestica@est.fi.uncoma.edu.ar>
2021-11-26 16:52:40 -0300

    footer y problema de scrolling solucionado

--- menu.js
+++ menu.js
@@ -6,5 +6,6 @@ menu.addEventListener('click', () => {
     menu.firstElementChild.classList.toggle('hide');
     menu.lastElementChild.classList.toggle('hide');
     wrapper.classList.toggle('active');
+    document.body.classList.toggle('stop-scrolling');
 
 });
