1405dc1089be03612b2bd80ae43a107fc558d524
JuanMartinMestica <juan.mestica@est.fi.uncoma.edu.ar>
2021-11-30 14:56:14 -0300

    se chequea timestamp antes de realizar una nueva consulta

--- routes/api.js
+++ routes/api.js
@@ -8,18 +8,12 @@ require('dotenv').config();
 let datos = {};
 
 router.post('/ciudades/:nombreCiudad', async (req, res) => {
-
+    
     const nombreCiudad = req.params.nombreciudad;
 
     if (datos[nombreCiudad] === undefined) {
 
-        const fetchres = await fetch(`https://community-open-weather-map.p.rapidapi.com/forecast?q=${nombreCiudad}&units=metric&lang=sp`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-                "x-rapidapi-key": process.env.API_KEY
-            }
-        });
+        const fetchres = await fetchAPI(nombreCiudad);
 
         if (fetchres.status !== 404) {
             const json = await fetchres.json();
@@ -30,19 +24,14 @@ router.post('/ciudades/:nombreCiudad', async (req, res) => {
 
 //Request de clima a partir de un nombre de ciudad
 router.get('/:nombreciudad', async (req, res) => {
+
     //Se extrae el nombre de ciudad
     const nombreCiudad = req.params.nombreciudad;
 
     //Se verifica si no existe en el diccionario la ciudad solicitada
     if (datos[nombreCiudad] === undefined) {
 
-        const fetchres = await fetch(`https://community-open-weather-map.p.rapidapi.com/forecast?q=${nombreCiudad}&units=metric&lang=sp`, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-                "x-rapidapi-key": process.env.API_KEY
-            }
-        });
+        let fetchres = await fetchAPI(nombreCiudad);
 
         //Se chequea si la API retorna 404 y se retorna al script del frontend
         if (fetchres.status === 404) {
@@ -56,15 +45,47 @@ router.get('/:nombreciudad', async (req, res) => {
             con los datos del clima para la ciudad */
             datos[nombreCiudad] = [json, Date.now()];
 
-            res.json(json);
         }
     } else {
-        
 
-        res.json(datos[nombreCiudad][0]);
+        //Se obtiene el timestamp de cuando se hizo la consulta
+        let timestamp = datos[nombreCiudad][1];
+        let hs = 3;
+        let cantMs = hs * 3600000;
 
+        //Se verifica si pasaron 3 horas desde la última consulta
+        if (Date.now() - timestamp > cantMs) {
+
+            console.log('Se actualizaron los datos');
+
+            /*Si pasaron más de 3 horas desde la anterior consulta 
+            entonces se hace una nueva consulta y se actualiza el diccionario local*/
+            let fetchres = await fetchAPI(nombreCiudad);
+
+            const json = await fetchres.json();
+
+            datos[nombreCiudad] = [json, Date.now()];
+
+        }
     }
+
+    //Se retornan los datos correspondientes
+    res.json(datos[nombreCiudad][0]);
 });
+
+const fetchAPI = async (nombreCiudad) => {
+
+    const fetchres = await fetch(`https://community-open-weather-map.p.rapidapi.com/forecast?q=${nombreCiudad}&units=metric&lang=sp`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+            "x-rapidapi-key": process.env.API_KEY
+        }
+    });
+
+    return fetchres;
+
+}
 
 
 module.exports = router;
